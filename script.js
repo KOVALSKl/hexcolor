b85a16a934f8d6c32dbc8c502d7667179af257ec
Bogdan Mikhaenko <s0160194@edu.kubsu.ru>
2022-02-02 23:48:42 +0300

    added random degrees

--- script.js
+++ script.js
@@ -4,10 +4,15 @@ let body = document.getElementById("wrapper");
 let span = document.getElementById("colorCode");
 
 btn.addEventListener("click", () => {
-    span.innerHTML = `linear-gradient(-60deg, ${getRandomColor()}, ${getRandomColor()})`;
+    span.innerHTML = `linear-gradient(${getRandomDegrees()}, ${getRandomColor()}, ${getRandomColor()})`;
     body.style.background = span.innerHTML;
 });
 
 function getRandomColor() {
     return "#" + Math.floor(Math.random()*16777215).toString(16);
+}
+
+function getRandomDegrees() {
+    let rand = Math.round(-0.5 + Math.random() * 181);
+    return ((rand % 2 == 0) ? rand : -rand).toString() + "deg";
 }
